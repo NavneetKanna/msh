52bc9e3fa243e3194cc7f535c759cb95fb973aa1
NavneetKanna <navneetkanna04@gmail.com>
2024-01-18 21:16:06 +0530

    moved it to a new header file

---
+++ len_of_command.h
@@ -0,0 +1,9 @@
+#ifndef READ_INP
+#define READ_INP
+
+typedef struct {
+    int start;
+    int end;
+} length_of_command;
+
+#endif
--- read_command.h
+++ read_command.h
@@ -1,11 +1,6 @@
 #ifndef READ_INP
 #define READ_INP
 
-typedef struct {
-    int start;
-    int end;
-} length_of_command;
-
 char *read_command();
 
 #endif
